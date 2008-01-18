8f58018cea1e943902b977a8b03bf1c57fb2cdb8
dochez <dochez@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2008-01-18 17:51:55 +0000

    Reorganized the GrizzlyAapter into 3 classes, register one adapter per http-listener as defined in the domain.xml, added support for virtual servers. fixed issue with deploying war files with persistence.xml cleaned up WebDeployer to use the new virtual server support.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@17756 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- v3/common/common-util/src/main/java/com/sun/enterprise/util/StringUtils.java
+++ v3/common/common-util/src/main/java/com/sun/enterprise/util/StringUtils.java
@@ -470,7 +470,7 @@ public class StringUtils
      * @return     Returns the list containing the individual strings that
      *             the input string was split into.
      */
-    public static List parseStringList(String line, String sep)
+    public static List<String> parseStringList(String line, String sep)
     {
         if (line == null)
             return null;
@@ -483,7 +483,7 @@ public class StringUtils
 
         String token;
 
-        List tokens = new Vector();
+        List<String> tokens = new Vector();
         while (st.hasMoreTokens())
         {
             token = st.nextToken().trim();
