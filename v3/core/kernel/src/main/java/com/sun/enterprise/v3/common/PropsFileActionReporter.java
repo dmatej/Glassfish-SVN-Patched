809812d2f3d589da4d5371e203dc3702775cd016
janey <janey@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2008-03-24 21:18:38 +0000

    Remove masking of " " to "_".  This is needed since the original code is changing the child type of getMessage from space to "_".   With the new code change, output will look like this:  "hello1_<web>",  instead of this:  "hello1 <web>". Change property separator to ";" instead of ",".  This is so that "," can be used in the output to separate the sniffer engine in "<...>".   I've tested that the output of list-* commands are not impacted by this change.  Instead, the new code change will make the output legible.  In the future, I feel this portion of the code should use XMLReporter instead of a customized reporter which is received by the client as java.util.jar.Manifest object.  The reason being XML format is more recognized by developers.
    Reviewed by: Hong Zhang
    
    passed QL and manual tests
    
    
    
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@19203 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/common/PropsFileActionReporter.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/common/PropsFileActionReporter.java
@@ -1,24 +1,34 @@
 /*
- * The contents of this file are subject to the terms 
- * of the Common Development and Distribution License 
- * (the License).  You may not use this file except in
- * compliance with the License.
- * 
- * You can obtain a copy of the license at 
- * https://glassfish.dev.java.net/public/CDDLv1.0.html or
- * glassfish/bootstrap/legal/CDDLv1.0.txt.
- * See the License for the specific language governing 
- * permissions and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL 
- * Header Notice in each file and include the License file 
- * at glassfish/bootstrap/legal/CDDLv1.0.txt.  
- * If applicable, add the following below the CDDL Header, 
- * with the fields enclosed by brackets [] replaced by
- * you own identifying information: 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 
 package com.sun.enterprise.v3.common;
@@ -57,18 +67,19 @@ public class PropsFileActionReporter extends ActionReporter {
             String keys=null;
             for (Map.Entry entry : part.getProps().entrySet()) {
                 String key  = entry.getKey().toString().replaceAll(" ", "_");
-                keys = (keys==null?key:keys + "," + key);
+                keys = (keys==null?key:keys + ";" + key);
                 attr.putValue(key+"_name", entry.getKey().toString());
                 attr.putValue(key+"_value", entry.getValue().toString());
             }
+         
             attr.putValue("keys", keys);
         }
         if (part.getChildren().size()>0) {
             attr.putValue("children-type", part.getChildrenType());
             String keys=null;
             for (MessagePart child : part.getChildren()) {
-                String newPrefix = (prefix==null?child.getMessage():prefix+"."+child.getMessage()).replaceAll(" ", "_");
-                keys = (keys==null?newPrefix:keys + "," + newPrefix);
+                String newPrefix = (prefix==null?child.getMessage():prefix+"."+child.getMessage());
+                keys = (keys==null?newPrefix:keys + ";" + newPrefix);
                 Attributes childAttr = new Attributes();
                 m.getEntries().put(newPrefix, childAttr);
                 writeReport(newPrefix, child, m, childAttr);
