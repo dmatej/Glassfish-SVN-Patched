ebe564f308852213c293db90a3397b1099cc2702
vivekp <vivekp@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2008-04-03 17:47:46 +0000

    Fix related to bootstrap.Main rename to bootstrap.ASMain.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@19576 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- v3/distributions/gem/glassfish.rb
+++ v3/distributions/gem/glassfish.rb
@@ -1,9 +1,9 @@
 require 'glassfish-10.0-SNAPSHOT.jar'
 
 module GlassFish
-  import com.sun.enterprise.glassfish.bootstrap.Main
+  import com.sun.enterprise.glassfish.bootstrap.ASMain
   def startup(args)
-    Main.main(args.to_java(:string))
+    ASMain.main(args.to_java(:string))
   end
 
   module_function :startup
