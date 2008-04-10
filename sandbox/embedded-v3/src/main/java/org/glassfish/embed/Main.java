d20aefb69f96fe6faaa648341ace7f42f089412c
kohsuke <kohsuke@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2008-04-10 22:03:06 +0000

    making more progress.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@19796 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- sandbox/embedded-v3/src/main/java/org/glassfish/embed/Main.java
+++ sandbox/embedded-v3/src/main/java/org/glassfish/embed/Main.java
@@ -40,15 +40,22 @@ package org.glassfish.embed;
 import com.sun.enterprise.module.bootstrap.StartupContext;
 import com.sun.enterprise.module.bootstrap.BootException;
 import com.sun.enterprise.module.impl.ModulesRegistryImpl;
+import com.sun.enterprise.module.ModulesRegistry;
+import com.sun.enterprise.module.Module;
 import com.sun.enterprise.v3.server.DomainXml;
+import com.sun.enterprise.v3.services.impl.LogManagerService;
 import com.sun.hk2.component.InhabitantsParser;
 import com.sun.hk2.component.KeyValuePairParser;
+import com.sun.hk2.component.ExistingSingletonInhabitant;
+import com.sun.hk2.component.ConstructorWomb;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 
 import org.jvnet.hk2.component.Habitat;
 import org.jvnet.hk2.component.Inhabitant;
+import org.jvnet.hk2.component.MultiMap;
 
 /**
  * Launches a mock-up HK2 environment that doesn't provide
@@ -60,8 +67,15 @@ import org.jvnet.hk2.component.Inhabitant;
 public class Main extends com.sun.enterprise.module.bootstrap.Main {
     public static void main(String[] args) {
         try {
+            long start = System.currentTimeMillis();
             new Main().boot(args);
-            System.exit(0);
+
+            System.out.println("GFv3 started in "+(System.currentTimeMillis()-start)+"ms");
+            // block forever. TODO: show how to shut down v3 cleanly.
+            Object o = new Object();
+            synchronized (o) {
+                o.wait();
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
@@ -69,10 +83,21 @@ public class Main extends com.sun.enterprise.module.bootstrap.Main {
     }
 
     public void boot(String[] args) throws IOException, BootException {
-        ModulesRegistryImpl mrs = new ModulesRegistryImpl(null);
-        mrs.add(new ProxyModuleDefinition(Main.class.getClassLoader()));
+        final Module[] proxyMod = new Module[1];
+        ModulesRegistryImpl mrs = new ModulesRegistryImpl(null) {
+            public Module find(Class clazz) {
+                Module m = super.find(clazz);
+                if(m==null)
+                    return proxyMod[0];
+                return m;
+            }
+        };
+        proxyMod[0] = mrs.add(new ProxyModuleDefinition(Main.class.getClassLoader()));
 
         StartupContext startupContext = new StartupContext(new File("./temp"), args);
+
+        // HACK TO BE REMOVED
+        System.setProperty("com.sun.aas.instanceRoot",".");
 
         launch(mrs,startupContext);
     }
@@ -83,8 +108,20 @@ public class Main extends com.sun.enterprise.module.bootstrap.Main {
             protected void add(Inhabitant i, KeyValuePairParser kvpp) {
                 // we'll build the equivalent of domain.xml on our own,
                 // so don't let the default component parse it.
-                if(i.typeName().equals(DomainXml.class.getName()))
+//                if(i.typeName().equals(DomainXml.class.getName()))
+//                    return;
+
+                if(i.typeName().equals(LogManagerService.class.getName()))
                     return;
+
+                if(i.typeName().equals(DomainXml.class.getName())) {
+                    MultiMap metadata = new MultiMap(i.metadata());
+                    metadata.set("class", Collections.singletonList(DomainXmlParser2.class.getName()));
+                    super.add(new ConstructorWomb(DomainXmlParser2.class,habitat, metadata),kvpp);
+                    return;
+                }
+
+
                 super.add(i, kvpp);
             }
         };
