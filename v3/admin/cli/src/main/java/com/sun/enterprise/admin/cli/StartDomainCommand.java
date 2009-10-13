dmatej/Glassfish-SVN-Patched

commit 8a3cb1d5103c050b4dcf7332ecb722e9c4337e7c

If the domain doesn't have a <network-config> element, start the domain with -upgrade, then start it for real.
git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@32517 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

v3/admin/cli/src/main/java/com/sun/enterprise/admin/cli/StartDomainCommand.java
@@ -37,6 +37,7 @@
 package com.sun.enterprise.admin.cli;
 
 import java.io.*;
+import java.util.*;
 
 import org.jvnet.hk2.annotations.*;
 import org.jvnet.hk2.component.*;
@@ -52,8 +53,6 @@ import com.sun.enterprise.admin.launcher.GFLauncherInfo;
 import com.sun.enterprise.universal.i18n.LocalStringsImpl;
 import com.sun.enterprise.universal.process.ProcessStreamDrainer;
 import com.sun.enterprise.universal.xml.MiniXmlParserException;
-
-import java.util.*;
 
 /**
  * The start-domain command.
@@ -73,7 +72,10 @@ public class StartDomainCommand extends LocalDomainCommand {
 
     private static final LocalStringsImpl strings =
             new LocalStringsImpl(StartDomainCommand.class);
-    private static final int DEATH_TIMEOUT_MS = 5 * 60 * 1000; // 5 minute timeout should be plenty!
+    // 5 minute timeout should be plenty!
+    private static final int DEATH_TIMEOUT_MS = 5 * 60 * 1000;
+    // the name of the master password option
+    private static final String MASTER_PASSWORD = "AS_ADMIN_MASTER_PASSWORD";
 
     /**
      * The prepare method must ensure that the commandOpts,
@@ -112,21 +114,7 @@ public class StartDomainCommand extends LocalDomainCommand {
 
     private int runCommandNotEmbedded() throws CommandException {
         try {
-            launcher = GFLauncherFactory.getInstance(
-                    GFLauncherFactory.ServerType.domain);
-            info = launcher.getInfo();
-
-            info.setDomainName(domainName);
-            info.setDomainParentDir(domainsDir.getPath());
-            info.setVerbose(verbose || upgrade);
-            info.setDebug(debug);
-            info.setUpgrade(upgrade);
-
-            info.setRespawnInfo(programOpts.getClassName(),
-                            programOpts.getClassPath(),
-                            programOpts.getProgramArguments());
-
-            launcher.setup();
+            createLauncher(GFLauncherFactory.ServerType.domain);
 
             if (Boolean.getBoolean(RESTART_FLAG)) {
                 new DeathWaiter();
@@ -141,7 +129,45 @@ public class StartDomainCommand extends LocalDomainCommand {
 
             // this can be slow, 500 msec,
             // with --passwordfile option it is ~~ 18 msec
-            setMasterPassword(info);
+            String mpv = getMasterPassword();
+            info.addSecurityToken(MASTER_PASSWORD, mpv);
+
+            /*
+             * If this domain needs to be upgraded and --upgrade wasn't
+             * specified, first start the domain to do the upgrade and
+             * then start the domain again for real.
+             */
+            if (!upgrade && launcher.needsUpgrade()) {
+                logger.printMessage(strings.get("upgradeNeeded"));
+                info.setUpgrade(true);
+                launcher.setup();
+                launcher.launch();
+                Process p = launcher.getProcess();
+                int exitCode = -1;
+                try {
+                    exitCode = p.waitFor();
+                } catch (InterruptedException ex) {
+                    // should never happen
+                }
+                if (exitCode != SUCCESS) {
+                    ProcessStreamDrainer psd =
+                        launcher.getProcessStreamDrainer();
+                    String output = psd.getOutErrString();
+                    if (ok(output))
+                        throw new CommandException(
+                                strings.get("upgradeFailedOutput",
+                                    info.getDomainName(), exitCode, output));
+                    else
+                        throw new CommandException(strings.get("upgradeFailed",
+                                    info.getDomainName(), exitCode));
+                }
+                logger.printMessage(strings.get("upgradeSuccessful"));
+
+                // need a new launcher to start the domain for real
+                createLauncher(GFLauncherFactory.ServerType.domain);
+                info.addSecurityToken(MASTER_PASSWORD, mpv);
+                // continue with normal start...
+            }
 
             // launch returns very quickly if verbose is not set
             // if verbose is set then it returns after the domain dies
@@ -170,32 +196,39 @@ public class StartDomainCommand extends LocalDomainCommand {
         }
     }
 
-    private void setMasterPassword1(GFLauncherInfo info)
-                                throws CommandException {
-        // Sets the password into the launcher info.
-        // Yes, setting master password into a string is not right ...
-        long t0 = System.currentTimeMillis();
-        String mpn  = "AS_ADMIN_MASTERPASSWORD";
-        String mpv  = passwords.get(mpn);
-        if (mpv == null)
-            mpv = readFromMasterPasswordFile();
-        if (mpv == null)
-            mpv = "changeit"; //the default
-        boolean ok = verifyMasterPassword(mpv);
-        long t1 = System.currentTimeMillis();
-        logger.printDebugMessage("Master Password processing took: " + (t1-t0) + " msec");
-        if (!ok) {
-            mpv = retry(3);
-        }
-        info.addSecurityToken(mpn, mpv);
+    /**
+     * Create a launcher for the domain specified by arguments to
+     * this command.  The launcher is for a server of the specified type.
+     * Sets the launcher and info fields.
+     */
+    private void createLauncher(GFLauncherFactory.ServerType type)
+                        throws GFLauncherException, MiniXmlParserException {
+            launcher = GFLauncherFactory.getInstance(type);
+            info = launcher.getInfo();
+
+            info.setDomainName(domainName);
+            info.setDomainParentDir(domainsDir.getPath());
+            info.setVerbose(verbose || upgrade);
+            info.setDebug(debug);
+            info.setUpgrade(upgrade);
+
+            info.setRespawnInfo(programOpts.getClassName(),
+                            programOpts.getClassPath(),
+                            programOpts.getProgramArguments());
+
+            launcher.setup();
     }
-    private void setMasterPassword(GFLauncherInfo info) throws CommandException {
+
+    /**
+     * Get the master password, either from a password file or
+     * by asking the user.
+     */
+    private String getMasterPassword() throws CommandException {
         // Sets the password into the launcher info.
-        // Yes, setting master password into a string is not right ...
+        // Yes, returning master password as a string is not right ...
         final int RETRIES = 3;
         long t0 = System.currentTimeMillis();
-        String mpn  = "AS_ADMIN_MASTERPASSWORD";
-        String mpv  = passwords.get(mpn);
+        String mpv  = passwords.get(MASTER_PASSWORD);
         if (mpv == null) { //not specified in the password file
             mpv = "changeit";  //optimization for the default case -- see 9592
             if (!verifyMasterPassword(mpv)) {
@@ -204,13 +237,14 @@ public class StartDomainCommand extends LocalDomainCommand {
                     mpv = retry(RETRIES);
                 }
             }
-        } else { //the passwordfile contains AS_ADMIN_MASTERPASSWORD, use it at once
-            if(!verifyMasterPassword(mpv))
+        } else { // the passwordfile contains AS_ADMIN_MASTERPASSWORD, use it
+            if (!verifyMasterPassword(mpv))
                 mpv = retry(RETRIES);
         }
-        info.addSecurityToken(mpn, mpv);
         long t1 = System.currentTimeMillis();
-        logger.printDebugMessage("Time spent in master password extraction: " + (t1-t0) + " msec");       //TODO
+        logger.printDebugMessage("Time spent in master password extraction: " +
+                                    (t1-t0) + " msec");       //TODO
+        return mpv;
     }
 
     private String retry(int times) throws CommandException {
@@ -237,14 +271,7 @@ public class StartDomainCommand extends LocalDomainCommand {
 
     private int runCommandEmbedded() throws CommandException {
         try {
-            launcher = GFLauncherFactory.getInstance(
-                    GFLauncherFactory.ServerType.embedded);
-            info = launcher.getInfo();
-            info.setDomainName(domainName);
-            info.setDomainParentDir(domainsDir.getPath());
-            info.setVerbose(verbose);
-            info.setDebug(debug);
-            launcher.setup();
+            createLauncher(GFLauncherFactory.ServerType.embedded);
 
             // now admin ports are set.
             Set<Integer> ports = info.getAdminPorts();

v3/admin/cli/src/main/java/com/sun/enterprise/admin/cli/LocalStrings.properties
@@ -137,6 +137,10 @@ mp.prompt=Enter master password ({0}) attempt(s) remain)>
 no.console=No console, no prompting possible
 retry.mp=Sorry, incorrect master password, retry
 mp.giveup=Number of attempts ({0}) exhausted, giving up
+upgradeNeeded=Domain needs to be upgraded before it can be started.\nAttempting upgrade now...
+upgradeSuccessful=Upgrade was successful, starting domain...
+upgradeFailed=Error upgrading domain: {0}.\nThe server exited prematurely with exit code {1}.
+upgradeFailedOutput=Error upgrading domain: {0}.\nThe server exited prematurely with exit code {1}.\nBefore it died, it produced the following output:\n\n{2}
 
 #copy of those from cli-framework
 Usage=Usage: {0}

v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncher.java
@@ -150,6 +150,8 @@ public abstract class GFLauncher {
         setClasspath();
         setCommandLine();
         logCommandLine();
+        // if no <network-config> element, we need to upgrade this domain
+        needsUpgrade = !parser.hasNetworkConfig();
         setupCalledByClients = true;
     }
 
@@ -231,6 +233,15 @@ public abstract class GFLauncher {
             }
         }
         return -1;
+    }
+
+    /**
+     * Does this domain need to be upgraded before it can be started?
+     *
+     * @return true if the domain needs to be upgraded first
+     */
+    public boolean needsUpgrade() {
+        return needsUpgrade;
     }
     
     ///////////////////////////////////////////////////////////////////////////
@@ -729,6 +740,7 @@ public abstract class GFLauncher {
     private Process process;
     private ProcessStreamDrainer    psd;
     private boolean logFilenameWasFixed = false;
+    private boolean needsUpgrade = false;
 
     ///////////////////////////////////////////////////////////////////////////
     

v3/common/common-util/src/main/java/com/sun/enterprise/universal/xml/MiniXmlParser.java
@@ -1,25 +1,39 @@
 /*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License).  You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the license at
- * https://glassfish.dev.java.net/public/CDDLv1.0.html or
- * glassfish/bootstrap/legal/CDDLv1.0.txt.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at glassfish/bootstrap/legal/CDDLv1.0.txt.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * you own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008-2009 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
+
 package com.sun.enterprise.universal.xml;
 
 import com.sun.common.util.logging.LoggingConfigImpl;
@@ -177,6 +191,10 @@ public class MiniXmlParser {
         return monitoringEnabled;
     }
 
+    public boolean hasNetworkConfig() {
+        return sawNetworkConfig;
+    }
+
     /////////////////////  all private below  /////////////////////////
     
     private void read() throws XMLStreamException, EndDocumentException, FileNotFoundException {
@@ -294,6 +312,7 @@ public class MiniXmlParser {
                 } else if ("http-service".equals(name)) {
                     parseHttpService();
                 } else if ("network-config".equals(name)) {
+                    sawNetworkConfig = true;
                     parseListeners();
                 } else if ("monitoring-service".equals(name)) {
                     parseMonitoringService();
@@ -708,4 +727,5 @@ public class MiniXmlParser {
     private boolean monitoringEnabled;
     private List<Map<String, String>> vsAttributes = new ArrayList<Map<String, String>>();
     private List<Map<String, String>> listenerAttributes = new ArrayList<Map<String, String>>();
+    private boolean sawNetworkConfig;
 }

v3/common/common-util/src/test/java/com/sun/enterprise/universal/xml/MiniXmlParserTest.java
@@ -1,3 +1,39 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package com.sun.enterprise.universal.xml;
 
 import java.io.File;
@@ -329,6 +365,19 @@ public class MiniXmlParserTest {
             new File(getClass().getClassLoader().getResource("olddomain.xml").getPath()), "server");
         Set<Integer> ports = parser.getAdminPorts();
         assertEquals(1, ports.size());
+    }
+
+    @Test
+    public void testNoNetworkConfig() throws MiniXmlParserException {
+        final MiniXmlParser parser = new MiniXmlParser(
+            new File(getClass().getClassLoader().getResource("olddomain.xml").getPath()), "server");
+        assert(!parser.hasNetworkConfig());
+    }
+
+    @Test
+    public void testNetworkConfig() throws MiniXmlParserException {
+        final MiniXmlParser parser = new MiniXmlParser(rightOrder, "server");
+        assert(parser.hasNetworkConfig());
     }
 
     @Test

commit 14e1c99ed513ca2662a4aaa8a6e726534de0a73a

Fixing a regression caused in r32517. This results in failing to start a domain with non-default master-password. We should really have unit tests for this ...
git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@32722 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

v3/admin/cli/src/main/java/com/sun/enterprise/admin/cli/StartDomainCommand.java
@@ -76,7 +76,7 @@ public class StartDomainCommand extends LocalDomainCommand {
     // 5 minute timeout should be plenty!
     private static final int DEATH_TIMEOUT_MS = 5 * 60 * 1000;
     // the name of the master password option
-    private static final String MASTER_PASSWORD = "AS_ADMIN_MASTER_PASSWORD";
+    private static final String MASTER_PASSWORD = "AS_ADMIN_MASTERPASSWORD";
 
     /**
      * The prepare method must ensure that the commandOpts,