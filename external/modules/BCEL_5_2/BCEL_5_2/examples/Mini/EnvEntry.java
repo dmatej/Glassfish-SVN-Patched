f4495e4c18f3711538f83ebd9c2210cf3356c589
ne110415 <ne110415@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2009-12-03 08:44:17 +0000

    Exporting the BCEL_5_2 sources to GlassFish SVN repository
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@34875 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ external/modules/BCEL_5_2/BCEL_5_2/examples/Mini/EnvEntry.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright  2000-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ *
+ */
+package Mini;
+
+/**
+ * Entry in environment.
+ *
+ * @version $Id: EnvEntry.java 410087 2006-05-29 12:12:19Z tcurdt $
+ * @author <A HREF="mailto:m.dahm@gmx.de">M. Dahm</A>
+ */
+public interface EnvEntry {
+  public String getHashKey();
+  public int    getLine();
+  public int    getColumn();
+}
