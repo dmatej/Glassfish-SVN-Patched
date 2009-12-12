4a080b71cfbfdea47d3b5e45efcecb7ce7abda48
mm110999 <mm110999@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2009-12-12 01:45:07 +0000

    Adding sources from antlr for build from souce
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@35095 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ external/modules/antlr/2.7.6/src/main/java/antlr/FileCopyException.java
@@ -0,0 +1,14 @@
+package antlr;
+
+/* ANTLR Translator Generator
+ * Project led by Terence Parr at http://www.cs.usfca.edu
+ * Software rights: http://www.antlr.org/license.html
+ *
+ * $Id: //depot/code/org.antlr/release/antlr-2.7.6/antlr/FileCopyException.java#1 $
+ */
+
+class FileCopyException extends java.io.IOException {
+    public FileCopyException(String msg) {
+        super(msg);
+    }
+}
