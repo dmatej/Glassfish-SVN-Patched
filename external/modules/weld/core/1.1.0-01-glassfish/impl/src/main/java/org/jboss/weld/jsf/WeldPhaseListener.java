0b5c9ca5a0ab6046f4a730bdcbc0aa81eed38652
sivakumart <sivakumart@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2010-07-19 16:24:00 +0000

    Adding the sources for the 1.1.0-01-glassfish tag of Weld 1.1 Core workspace source: http://anonsvn.jboss.org/repos/weld/core/tags/1.1.0-01-glassfish/
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@38819 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ external/modules/weld/core/1.1.0-01-glassfish/impl/src/main/java/org/jboss/weld/jsf/WeldPhaseListener.java
@@ -0,0 +1,162 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.jsf;
+
+import static org.jboss.weld.jsf.JsfHelper.getConversationId;
+import static org.jboss.weld.jsf.JsfHelper.getServletContext;
+import static org.jboss.weld.logging.Category.JSF;
+import static org.jboss.weld.logging.LoggerFactory.loggerFactory;
+import static org.jboss.weld.logging.messages.JsfMessage.CLEANING_UP_CONVERSATION;
+import static org.jboss.weld.logging.messages.JsfMessage.INITIATING_CONVERSATION;
+import static org.jboss.weld.servlet.BeanProvider.conversationManager;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import org.jboss.weld.conversation.ConversationManager2;
+import org.slf4j.cal10n.LocLogger;
+
+/**
+ * <p>
+ * A JSF phase listener that initializes aspects of Weld in a more fine-grained,
+ * integrated manner than what is possible with a servlet filter. This phase
+ * listener works in conjunction with other hooks and callbacks registered with
+ * the JSF runtime to help manage the Weld lifecycle.
+ * </p>
+ * 
+ * <p>
+ * It's expected that over time, this phase listener may take on more work, but
+ * for now the work is focused solely on conversation management. The phase
+ * listener restores the long-running conversation if the conversation id token
+ * is detected in the request, activates the conversation context in either case
+ * (long-running or transient), and finally passivates the conversation after
+ * the response has been committed.
+ * </p>
+ * 
+ * @author Nicklas Karlsson
+ * @author Dan Allen
+ */
+public class WeldPhaseListener implements PhaseListener
+{
+   private static final long serialVersionUID = 1L;
+
+   private static final LocLogger log = loggerFactory().getLogger(JSF);
+
+   private ConversationManager2 conversationManager;
+
+   private ConversationManager2 getConversationManager()
+   {
+      if (conversationManager == null)
+      {
+         conversationManager = conversationManager(getServletContext(FacesContext.getCurrentInstance()));
+      }
+      return conversationManager;
+   }
+
+   /**
+    * Execute before every phase in the JSF life cycle. The order this phase
+    * listener executes in relation to other phase listeners is determined by
+    * the ordering of the faces-config.xml descriptors. This phase listener
+    * should take precedence over extensions.
+    * 
+    * @param phaseEvent The phase event
+    */
+   public void beforePhase(PhaseEvent phaseEvent)
+   {
+      if (phaseEvent.getPhaseId().equals(PhaseId.RESTORE_VIEW))
+      {
+         beforeRestoreView(phaseEvent.getFacesContext());
+      }
+   }
+
+   /**
+    * Execute after every phase in the JSF life cycle. The order this phase
+    * listener executes in relation to other phase listeners is determined by
+    * the ordering of the faces-config.xml descriptors. This phase listener
+    * should take precedence over extensions.
+    * 
+    * @param phaseEvent The phase event
+    */
+   public void afterPhase(PhaseEvent phaseEvent)
+   {
+      if (phaseEvent.getPhaseId().equals(PhaseId.RENDER_RESPONSE))
+      {
+         afterRenderResponse(phaseEvent.getFacesContext());
+      }
+      // be careful with this else as it assumes only one if condition right now
+      else if (phaseEvent.getFacesContext().getResponseComplete())
+      {
+         afterResponseComplete(phaseEvent.getFacesContext(), phaseEvent.getPhaseId());
+      }
+   }
+
+   /**
+    * Execute before the Restore View phase.
+    */
+   private void beforeRestoreView(FacesContext facesContext)
+   {
+      log.trace(INITIATING_CONVERSATION, "Restore View");
+      //initiateSessionAndConversation(facesContext);
+   }
+
+   /**
+    * Execute after the Render Response phase.
+    */
+   private void afterRenderResponse(FacesContext facesContext)
+   {
+      log.trace(CLEANING_UP_CONVERSATION, "Render Response", "response complete");
+      //getConversationManager().teardownConversation();
+   }
+
+   /**
+    * Execute after any phase that marks the response as complete.
+    */
+   private void afterResponseComplete(FacesContext facesContext, PhaseId phaseId)
+   {
+      log.trace(CLEANING_UP_CONVERSATION, phaseId, "the response has been marked complete");
+      //getConversationManager().teardownConversation();
+   }
+
+   /**
+    * Retrieve the HTTP session from the FacesContext and assign it to the Web
+    * Beans HttpSessionManager. Restore the long-running conversation if the
+    * conversation id token is present in the request and, in either case,
+    * activate the conversation context (long-running or transient).
+    */
+   private void initiateSessionAndConversation(FacesContext facesContext)
+   {
+      getConversationManager().setupConversation(getConversationId(facesContext));
+   }
+
+   /**
+    * The phase id for which this phase listener is active. This phase listener
+    * observes all JSF life-cycle phases.
+    */
+   public PhaseId getPhaseId()
+   {
+      return PhaseId.ANY_PHASE;
+   }
+
+}
---
+++ external/modules/weld/core/1.1.0-01-glassfish/impl/src/main/java/org/jboss/weld/util/Names.java
@@ -0,0 +1,297 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.util;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utility class to produce friendly names e.g. for debugging
+ * 
+ * @author Pete Muir
+ * @author Nicklas Karlsson
+ * 
+ */
+public class Names
+{
+   // Pattern for recognizing strings with leading capital letter
+   private static Pattern CAPITAL_LETTERS = Pattern.compile("\\p{Upper}{1}\\p{Lower}*");
+
+   /**
+    * Gets a string representation of the scope type annotation
+    * 
+    * @param scopeType The scope type
+    * @return A string representation
+    */
+   public static String scopeTypeToString(Class<? extends Annotation> scopeType)
+   {
+      StringBuilder result = new StringBuilder();
+      if (scopeType != null)
+      {
+         String scopeName = scopeType.getSimpleName();
+         Matcher matcher = CAPITAL_LETTERS.matcher(scopeName);
+         int i = 0;
+         while (matcher.find())
+         {
+            String name = matcher.group();
+            if (i > 0)
+            {
+               name = name.toLowerCase();
+            }
+            result.append(name).append(" ");
+            i++;
+         }
+      }
+      return result.toString();
+   }
+
+   public static String classToString(Class<?> rawType, Set<Annotation> annotations, Type[] actualTypeArguments)
+   {
+      return new NamesStringBuilder("class").add(modifiersToString(rawType.getModifiers())).add(annotationsToString(annotations)).add(rawType.getName()).add(typesToString(actualTypeArguments)).toString();
+   }
+
+   public static String fieldToString(Field field, Set<Annotation> annotations, Type[] actualTypeArguments)
+   {
+      return new NamesStringBuilder("field in " + field.getDeclaringClass().getName()).add(modifiersToString(field.getModifiers())).add(annotationsToString(annotations)).add(field.getType().getName()).add(typesToString(actualTypeArguments)).add(field.getName()).toString();
+   }
+
+   public static String methodToString(Method method, Set<Annotation> annotations, Type[] actualTypeArguments, List<?> parameters)
+   {
+      return new NamesStringBuilder("method in " + method.getDeclaringClass().getName()).add(modifiersToString(method.getModifiers())).add(annotationsToString(annotations)).add(method.getName()).add(typesToString(actualTypeArguments)).add(parametersToString(parameters)).toString();
+   }
+
+   private static String parametersToString(List<?> parameters)
+   {
+      return "(" + iterableToString(parameters, ", ") + ")";
+   }
+
+   public static String constructorToString(Constructor<?> constructor, Set<Annotation> annotations, Type[] actualTypeArguments, List<?> parameters)
+   {
+      return new NamesStringBuilder("constructor for " + constructor.getDeclaringClass().getName()).add(modifiersToString(constructor.getModifiers())).add(annotationsToString(annotations)).add(constructor.getName()).add(typesToString(actualTypeArguments)).add(parametersToString(parameters)).toString();
+   }
+
+   public static String parameterToString(Class<?> rawType, Set<Annotation> annotations, Type[] actualTypeArguments)
+   {
+      return new NamesStringBuilder().add(modifiersToString(rawType.getModifiers())).add(annotationsToString(annotations)).add(rawType.getName()).add(typesToString(actualTypeArguments)).toString();
+   }
+
+   /**
+    * Counts item in an iteratble
+    * 
+    * @param iterable The iteraboe
+    * @return The count
+    */
+   public static int count(final Iterable<?> iterable)
+   {
+      int count = 0;
+      for (Iterator<?> i = iterable.iterator(); i.hasNext();)
+      {
+         count++;
+      }
+      return count;
+   }
+
+   private static String modifiersToString(int modifiers)
+   {
+      return iterableToString(parseModifiers(modifiers), " ");
+   }
+
+   private static String iterableToString(Iterable<?> items, String delimiter)
+   {
+      StringBuffer stringBuffer = new StringBuffer();
+      int i = 0;
+      for (Object item : items)
+      {
+         if (i > 0)
+         {
+            stringBuffer.append(delimiter);
+         }
+         stringBuffer.append(item);
+         i++;
+      }
+      return stringBuffer.toString();
+   }
+
+   /**
+    * Parses a reflection modifier to a list of string
+    * 
+    * @param modifier The modifier to parse
+    * @return The resulting string list
+    */
+   private static List<String> parseModifiers(int modifier)
+   {
+      List<String> modifiers = new ArrayList<String>();
+      if (Modifier.isPrivate(modifier))
+      {
+         modifiers.add("private");
+      }
+      if (Modifier.isProtected(modifier))
+      {
+         modifiers.add("protected");
+      }
+      if (Modifier.isPublic(modifier))
+      {
+         modifiers.add("public");
+      }
+      if (Modifier.isAbstract(modifier))
+      {
+         modifiers.add("abstract");
+      }
+      if (Modifier.isFinal(modifier))
+      {
+         modifiers.add("final");
+      }
+      if (Modifier.isNative(modifier))
+      {
+         modifiers.add("native");
+      }
+      if (Modifier.isStatic(modifier))
+      {
+         modifiers.add("static");
+      }
+      if (Modifier.isStrict(modifier))
+      {
+         modifiers.add("strict");
+      }
+      if (Modifier.isSynchronized(modifier))
+      {
+         modifiers.add("synchronized");
+      }
+      if (Modifier.isTransient(modifier))
+      {
+         modifiers.add("transient");
+      }
+      if (Modifier.isVolatile(modifier))
+      {
+         modifiers.add("volatile");
+      }
+      if (Modifier.isInterface(modifier))
+      {
+         modifiers.add("interface");
+      }
+      return modifiers;
+   }
+
+   public static String typesToString(Type[] actualTypeArguments)
+   {
+      if (actualTypeArguments.length == 0)
+      {
+         return "";
+      }
+      StringBuilder buffer = new StringBuilder();
+      int i = 0;
+      buffer.append("<");
+      for (Type type : actualTypeArguments)
+      {
+         if (i > 0)
+         {
+            buffer.append(", ");
+         }
+         if (type instanceof Class<?>)
+         {
+            buffer.append(((Class<?>) type).getSimpleName());
+         }
+         else
+         {
+            buffer.append(type.toString());
+         }
+         i++;
+      }
+      buffer.append(">");
+      return buffer.toString();
+   }
+
+   public static String annotationsToString(Iterable<Annotation> annotations)
+   {
+      StringBuilder builder = new StringBuilder();
+      for (Annotation annotation : annotations)
+      {
+         builder.append(" ");
+         builder.append("@").append(annotation.annotationType().getSimpleName());
+      }
+      return builder.toString().trim();
+   }
+
+   /**
+    * Gets a string representation from an array of annotations
+    * 
+    * @param annotations The annotations
+    * @return The string representation
+    */
+   public static String annotationsToString(Annotation[] annotations)
+   {
+      return annotationsToString(Arrays.asList(annotations));
+   }
+
+   public static String version(Package pkg)
+   {
+      if (pkg == null)
+      {
+         throw new IllegalArgumentException("Package can not be null");
+      }
+      else
+      {
+         return version(pkg.getImplementationVersion());
+      }
+   }
+
+   public static String version(String version)
+   {
+      if (version != null)
+      {
+         StringBuilder builder = new StringBuilder();
+         if (version.indexOf(".") > 0)
+         {
+            builder.append(version.substring(0, version.indexOf("."))).append(".");
+            version = version.substring(version.indexOf(".") + 1);
+         }
+         if (version.indexOf(".") > 0)
+         {
+            builder.append(version.substring(0, version.indexOf("."))).append(".");
+            version = version.substring(version.indexOf(".") + 1);
+         }
+         if (version.indexOf("-") > 0)
+         {
+            builder.append(version.substring(0, version.indexOf("-"))).append(" (");
+            builder.append(version.substring(version.indexOf("-") + 1)).append(")");
+         }
+         else if (version.indexOf(".") > 0)
+         {
+            builder.append(version.substring(0, version.indexOf("."))).append(" (");
+            builder.append(version.substring(version.indexOf(".") + 1)).append(")");
+         }
+         else
+         {
+            builder.append(version);
+         }
+         return builder.toString();
+      }
+      return "SNAPSHOT";
+   }
+
+}
