52d6e59283c4db9c94acb7845e5f70fd2558505e
mk111283 <mk111283@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2010-09-08 17:03:32 +0000

    Fix copyright
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@40568 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- api/ha-api/src/test/java/org/glassfish/ha/api/test/BackingStoreFactoryRegistryTest.java
+++ api/ha-api/src/test/java/org/glassfish/ha/api/test/BackingStoreFactoryRegistryTest.java
@@ -1,27 +1,31 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
  *
  * Contributor(s):
- *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -203,48 +207,38 @@ public class BackingStoreFactoryRegistryTest
     private static final class NoopData
         implements Storeable {
 
-        @Override
         public long _storeable_getVersion() {
             return 0;
         }
 
-        @Override
         public void _storeable_setVersion(long version) {
         }
 
-        @Override
         public long _storeable_getLastAccessTime() {
             return 0;
         }
 
-        @Override
         public void _storeable_setLastAccessTime(long version) {
         }
 
-        @Override
         public long _storeable_getMaxIdleTime() {
             return 0;
         }
 
-        @Override
         public void _storeable_setMaxIdleTime(long version) {
         }
 
-        @Override
         public String[] _storeable_getAttributeNames() {
             return new String[0];
         }
 
-        @Override
         public boolean[] _storeable_getDirtyStatus() {
             return new boolean[0];
         }
 
-        @Override
         public void _storeable_writeState(OutputStream os) throws IOException {
         }
 
-        @Override
         public void _storeable_readState(InputStream is) throws IOException {
         }
     }
