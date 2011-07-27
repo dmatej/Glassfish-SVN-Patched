481f73b9b9457c82538bde8684b4078750bfbc75
janey <janey@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-07-27 13:48:54 +0000

    add meta-inf services source 1.2
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@48365 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ external/modules/metainf-services/1.2/src/main/java/org/kohsuke/MetaInfServices.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-, Kohsuke Kawaguchi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.kohsuke;
+
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that this class name should be listed into the <tt>META-INF/services/CONTRACTNAME</tt>.
+ *
+ * <p>
+ * If the class for which this annotation is placaed only have one base class or one interface,
+ * then the CONTRACTNAME is the fully qualified name of that type.
+ *
+ * <p>
+ * Otherwise, the {@link #value()} element is required to specify the contract type name.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Retention(SOURCE)
+@Documented
+@Target(TYPE)
+public @interface MetaInfServices {
+    Class value() default void.class;
+}
