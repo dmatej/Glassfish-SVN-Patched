e78674eb9c3d6fc74fb0abad95158570709468f1
shannon <shannon@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-09 23:14:19 +0000

    Ignore blank lines in copyright exclude file.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@51989 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- copyright-plugin/src/main/java/org/glassfish/copyright/Copyright.java
+++ copyright-plugin/src/main/java/org/glassfish/copyright/Copyright.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -241,6 +241,8 @@ public class Copyright {
     }
 
     public void addExclude(String ex) {
+	if (ex == null || ex.length() == 0)
+	    return;
 	if (debug)
 	    System.out.println("Add exclude: " + ex);
 	excludes.add(ex);
