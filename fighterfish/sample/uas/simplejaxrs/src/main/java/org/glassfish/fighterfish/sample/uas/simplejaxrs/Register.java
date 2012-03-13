d986e035fee084bd44512328be6aa398bbf5331a
vijay_oracle <vijay_oracle@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-03-13 05:48:06 +0000

    Sample JAX-RS Application
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@52887 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ fighterfish/sample/uas/simplejaxrs/src/main/java/org/glassfish/fighterfish/sample/uas/simplejaxrs/Register.java
@@ -0,0 +1,105 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.fighterfish.sample.uas.simplejaxrs;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+import org.glassfish.fighterfish.sample.uas.api.UserAuthService;
+import org.glassfish.osgicdi.OSGiService;
+import org.osgi.framework.ServiceException;
+
+/** Example resource class hosted at the URI path "/register"
+ */
+@Path("/register")
+@RequestScoped
+public class Register {
+	
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+	
+    @Inject @OSGiService(dynamic=true)
+    private UserAuthService uas;
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @POST 
+    @Produces("text/plain")
+    public String getIt() {
+        return "Hi there!";
+    }
+    
+    @GET
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String getLogin(
+			@Context HttpServletRequest request,
+			@Context HttpServletResponse response
+	) throws ServletException {
+	       String name = request.getParameter("name");
+	       String password = request.getParameter("password");
+	        try {
+       	            if (uas.register(name, password)) {
+                return "Registered";	  
+	            } else {
+		return "Failed";	        
+	            }
+	        } catch (ServiceException e) {
+                }
+       	return "none";
+		}
+}
