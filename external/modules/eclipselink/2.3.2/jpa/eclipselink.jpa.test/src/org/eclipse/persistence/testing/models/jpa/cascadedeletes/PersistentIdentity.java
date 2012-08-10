609bc21486960f6d60cb7df30d2d36a3d7be2537
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-08-10 10:58:01 +0000

    del jpa dir, commit did not succeed well
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@55386 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- external/modules/eclipselink/2.3.2/jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/cascadedeletes/PersistentIdentity.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.persistence.testing.models.jpa.cascadedeletes;
-
-public interface PersistentIdentity {
-    public int getId();
-
-}
--- external/modules/eclipselink/2.3.2/jpa/org.eclipse.persistence.jpa.jpql.test/resource/source/jpql/query/EnumType.java
+++
@@ -1,7 +0,0 @@
-package jpql.query;
-
-public enum EnumType {
-	FIRST_NAME,
-	LAST_NAME,
-	NAME
-}
