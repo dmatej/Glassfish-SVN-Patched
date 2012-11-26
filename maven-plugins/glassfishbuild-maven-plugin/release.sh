96cd7f2605bd4ebda9354b70834cef944e7ec5af
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-11-26 18:00:33 +0000

    adding a release.sh script
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@57103 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ maven-plugins/glassfishbuild-maven-plugin/release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#------------------------------------------------------   
+#--	BE SURE TO HAVE THE FOLLOWING IN YOUR SETTINGS.XML
+#------------------------------------------------------
+#
+#    <servers>
+#        <server>
+#            <id>jvnet-nexus-staging</id>
+#            <username>jvnet_id</username>
+#            <password>password</password>
+#        </server>
+#    </servers>
+
+# see the following URL for gpg issues
+# https://docs.sonatype.org/display/Repository/How+To+Generate+PGP+Signatures+With+Maven#HowToGeneratePGPSignaturesWithMaven-GenerateaKeyPair
+
+mvn -B release:prepare -DpreparationGoals="install -P\!jvnet-release"
+mvn -B release:perform -Dgoals="deploy -Dhttps.proxyHost=inet-rmmc01.oracle.com -Dhttps.proxyPort=80 -Dgpg.passphrase=glassfish"
