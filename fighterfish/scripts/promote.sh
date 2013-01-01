8af340a2908540767c37857e7343b90f530d023b
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-01 17:51:25 +0000

    Add scripts used to promote. Should have been included in last commit.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@57876 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ fighterfish/scripts/promote.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ex
+
+# MODULE is relative path of module to be promoted wrt fighterfish dir.
+# We hard code the module name to avoid having to update the hudson job
+# everytime we want to promote. This also allows us better tracking.
+MODULE=test/util
+
+if [ "$MODULE" = "" ]
+then
+ echo "Module name missing"
+ exit 1
+fi
+
+# Create a temporary dir to checkout the module(s) that need to be released.
+TS=`date +%F_%T`
+mkdir $WORKSPACE/$TS
+cd $WORKSPACE/$TS
+svn co https://svn.java.net/svn/glassfish~svn/trunk/fighterfish/$MODULE $MODULE
+
+cd $MODULE
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Prelease -B -DtagBase=https://svn.java.net/svn/glassfish~svn/tags/fighterfish-releases -DtagNameFormat=@{project.groupId}.@{project.artifactId}-@{project.version} release:prepare
+ 
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Prelease -B release:perform 
