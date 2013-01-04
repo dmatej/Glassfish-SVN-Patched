c2c009f561a5605a77c2bea0a1fd2f81ce92f69b
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-04 12:01:37 +0000

    We expect individual poms to setup skipping test part. We will also experiment by not using any profile during release.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@57944 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/scripts/promote.sh
+++ fighterfish/scripts/promote.sh
@@ -18,7 +18,6 @@ cd $WORKSPACE/$TS
 svn co https://svn.java.net/svn/glassfish~svn/trunk/fighterfish/$MODULE $MODULE
 
 cd $MODULE
-# Because of the way test/util is setup, we can't run tests while releasing it, so we have to skip tests.
-mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Prelease -B -DtagBase=https://svn.java.net/svn/glassfish~svn/tags/fighterfish-releases -DtagNameFormat=@{project.groupId}.@{project.artifactId}-@{project.version} release:prepare -Drelease.arguments=-DskipTests=true
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -B -DtagBase=https://svn.java.net/svn/glassfish~svn/tags/fighterfish-releases -DtagNameFormat=@{project.groupId}.@{project.artifactId}-@{project.version} release:prepare
  
-mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Prelease -B release:perform -Drelease.arguments=-DskipTests=true
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -B release:perform
