0de4a79a7cd2319d20cecfae66e7831c05fb5b3f
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-04 12:13:31 +0000

    Pass gpg passphrase while invoking release plugin.
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@57946 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/scripts/promote.sh
+++ fighterfish/scripts/promote.sh
@@ -11,6 +11,13 @@ then
  exit 1
 fi
 
+GPG_PASSPHRASE=$1
+if [ "$GPG_PASSPHRASE" = "" ]
+then
+ echo "GPG Passphrase must be provided or you must change the script to run in interactive mode."
+ exit 1
+fi
+
 # Create a temporary dir to checkout the module(s) that need to be released.
 TS=`date +%Y%m%d_%H%M%S`
 mkdir $WORKSPACE/$TS
@@ -18,6 +25,6 @@ cd $WORKSPACE/$TS
 svn co https://svn.java.net/svn/glassfish~svn/trunk/fighterfish/$MODULE $MODULE
 
 cd $MODULE
-mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -B -DtagBase=https://svn.java.net/svn/glassfish~svn/tags/fighterfish-releases -DtagNameFormat=@{project.groupId}.@{project.artifactId}-@{project.version} release:prepare
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -B -DtagBase=https://svn.java.net/svn/glassfish~svn/tags/fighterfish-releases -DtagNameFormat=@{project.groupId}.@{project.artifactId}-@{project.version} -Dgpg.passphrase=GPG_PASSPHRASE release:prepare
  
-mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -B release:perform
+mvn -Dmaven.repo.local=$WORKSPACE/repository -Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dgpg.passphrase=GPG_PASSPHRASE -B release:perform
