fb26088016643829b7681d4f78e4d9d466a90c41
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-06 09:40:51 +0000

    Prepare for release of uas/simpleservice
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@57998 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/scripts/promote.sh
+++ fighterfish/scripts/promote.sh
@@ -3,7 +3,7 @@
 # MODULE is relative path of module to be promoted wrt fighterfish dir.
 # We hard code the module name to avoid having to update the hudson job
 # everytime we want to promote. This also allows us better tracking.
-MODULE=sample/uas/api
+MODULE=sample/uas/simpleservice
 
 if [ "$MODULE" = "" ]
 then
