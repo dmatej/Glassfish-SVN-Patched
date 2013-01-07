237a206c785e7c24c9c00364699a65425987717e
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-07 04:38:55 +0000

    Prepare for release of sample/parent-pom
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@58059 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/scripts/promote.sh
+++ fighterfish/scripts/promote.sh
@@ -3,7 +3,7 @@
 # MODULE is relative path of module to be promoted wrt fighterfish dir.
 # We hard code the module name to avoid having to update the hudson job
 # everytime we want to promote. This also allows us better tracking.
-MODULE=test/parent-pom
+MODULE=sample/parent-pom
 
 if [ "$MODULE" = "" ]
 then
