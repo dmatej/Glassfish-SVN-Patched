f3a8ee108a67324d751e7503cb30e2247630020d
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-14 16:04:45 +0000

    changes for release purpose
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@58354 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- logging-annotation-processor/release.sh
+++ logging-annotation-processor/release.sh
@@ -16,14 +16,20 @@
 #        </server>
 #    </servers>
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 your-jvnet-password"
+if [ "$#" -ne 2 ] || [ "$#" -ne 0 ]
+then
+  echo "Usage: $0 your-jvnet-username your-jvnet-password"
   exit 1
+fi
+
+if [ "$#" -eq 2 ]
+then
+  CREDENTIALS="-Dusername=$1 -Dpassword=$2"
 fi
 
 # see the following URL for gpg issues
 # https://docs.sonatype.org/display/Repository/How+To+Generate+PGP+Signatures+With+Maven#HowToGeneratePGPSignaturesWithMaven-GenerateaKeyPair
 
-mvn -B release:prepare -DpreparationGoals="install -P\!jvnet-release" -Dpassword $1
-mvn -B release:perform -Dgoals="deploy -Dhttps.proxyHost=inet-rmmc01.oracle.com -Dhttps.proxyPort=80 -Pjvnet-release -Dgpg.passphrase=glassfish"
+mvn -B release:prepare -DpreparationGoals="install -P\!jvnet-release" $CREDENTIALS
+mvn -B release:perform -Dgoals="deploy -Dhttps.proxyHost=inet-rmmc01.oracle.com -Dhttps.proxyPort=80 -Pjvnet-release -Dgpg.passphrase=glassfish" $CREDENTIALS
 
