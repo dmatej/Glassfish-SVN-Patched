a5abc3c9f6f34602416bc784f94f8674e26ac312
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-02-23 18:33:48 +0000

    Fixed copyright year and formatting
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@59798 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/module/osgi-web-container/src/main/java/org/glassfish/osgiweb/OSGiWebDeploymentContext.java
+++ fighterfish/module/osgi-web-container/src/main/java/org/glassfish/osgiweb/OSGiWebDeploymentContext.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -231,7 +231,7 @@ class OSGiWebDeploymentContext extends OSGiDeploymentContext {
                         }))
                 {
                     //[TangYong]fixing GLASSFISH-19696
-                	  JarFile jarFile = null;
+                	JarFile jarFile = null;
                     try {
                     	jarFile = new JarFile(file);
                     	addJar(file.getPath().substring(baseFileLen), jarFile, file);
