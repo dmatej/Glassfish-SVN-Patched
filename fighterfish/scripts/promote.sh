d09401e37cf0f2d113210012598ccd6ad0a2696f
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-29 11:06:55 +0000

    prepare release of sample/parent-pom
    git-svn-id: svn+ssh://svn.java.net/glassfish~svn/trunk@61003 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- fighterfish/scripts/promote.sh
+++ fighterfish/scripts/promote.sh
@@ -5,7 +5,7 @@
 # We hard code the module name to avoid having to update the hudson job
 # everytime we want to promote. This also allows us better tracking.
 MODULES="\
- test/util \
+ sample/parent-pom \
 "
 
 GPG_PASSPHRASE=$1 #Take as argument for security reasons
